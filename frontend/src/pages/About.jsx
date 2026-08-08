26ede1dbbb72a34198f7205dd176c9ec8bd9cdea
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-08 14:09:44 +0100

    update the project structure

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,7 @@
+function About(){
+    return(<>
+    <h1>HELLO IN ABOUT PAGE</h1>
+    
+    </>)
+}
+export default About
---
+++ frontend/src/pages/Settings.jsx
@@ -0,0 +1,7 @@
+function Settings(){
+    return (<>
+    <h1>HELLO ON FAVORITES SETTINGS </h1>
+
+    </>)
+}
+export default Settings
