0ba86059b998ccc63b1ca388f71bab7158ad7314
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-10 01:20:57 +0100

    add states to searchbar

--- frontend/src/components/Header/Searchbar.jsx
+++ frontend/src/components/Header/Searchbar.jsx
@@ -1,16 +1,22 @@
 import "./Searchbar.css"
+import { useState } from "react"
 
-
-function Searchbar(){
+function Searchbar({onHandlesearch}){
+    const [city,setCity]=useState("")
+    const [err,setErr]=useState("")
+    function handleclick(e){
+        e.preventDefault()
+        if(!city.trim()){
+            setErr("tu dois remplir le champ")
+            return
+        }
+        onHandlesearch(city.trim())
+    }
     return (
         <div className="searchbar">
-            <input type = "text "placeholder="Search for a city..." />
-            <button>🔍</button>
-
+            <input onChange ={e=>setCity(e.target.value)} value={city} type = "text "placeholder="Search for a city..." />
+            <button onClick={handleclick} >🔍</button>
         </div>
-
-
     )
-
 }
 export default Searchbar
