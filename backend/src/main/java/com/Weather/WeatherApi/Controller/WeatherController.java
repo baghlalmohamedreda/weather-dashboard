d868a6486f65ed9e88f7e085f85e879d6e0e13e3
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-12 00:36:05 +0100

    fetch and display current weather from backend API

--- backend/src/main/java/com/Weather/WeatherApi/Controller/WeatherController.java
+++ backend/src/main/java/com/Weather/WeatherApi/Controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.Weather.WeatherApi.Controller;
 
 import com.Weather.WeatherApi.Model.dto.WeatherResponse;
 import com.Weather.WeatherApi.Service.WeatherService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/weather")
+@CrossOrigin(origins = "http://localhost:5173")
 public class WeatherController {
+
     private final WeatherService weatherService;
 
     public WeatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
     }
+
     @GetMapping
-    public WeatherResponse getCurrentWeather(@RequestParam String city){
+    public WeatherResponse getCurrentWeather(@RequestParam String city) {
         return weatherService.getCurrentWeatherByCity(city);
     }
 }
