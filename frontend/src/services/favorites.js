be2e253278180c680e2e6dd87c1fa5f27e108c08
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-15 18:46:56 +0100

    add favorites.js

---
+++ frontend/src/services/favorites.js
@@ -0,0 +1,21 @@
+
+
+export function getfavorites (){
+    return JSON.parse(localStorage.getItem("favoritescitys"))||[]
+
+}
+export function addcity(city){
+    const citys=getfavorites()
+    const isexist =citys.some(e=>e.city===city.city) 
+    if(isexist){
+        return
+    }
+    citys.push(city)
+    localStorage.setItem("favoritescitys",JSON.stringify(citys))
+
+}
+export function removecity(city){
+    const citys=getfavorites()
+    const updatecitys=citys.filter(e=>e.city!==city)
+    localStorage.setItem("favoritescitys",JSON.stringify(updatecitys))
+}
